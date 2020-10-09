0e54f3aebf68a0fca2bbd481c89d1d7a89b7147e
smax253 <smax253@gmail.com>
2020-10-09 16:33:03 -0400

    finish lab 5

--- lab5/routes/shows.js
+++ lab5/routes/shows.js
@@ -5,20 +5,19 @@ const router = express.Router();
 
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
-    if (!id.match(/[0-9]*/)) {
+    if (!id.match(/^[0-9]+$/)) {
         console.warn(`invalid id passed: ${id}`)
-        res.status(400).json({ error: 'id must be a positive whole number!' });
+        res.status(400).json({ error: 'id must be a positive whole number with only characters 0-9!' });
+        return;
     }
-    const showsData = (await axios.get('http://api.tvmaze.com/shows')).data;
-    //console.log(showsData);
-    const result = showsData.find((show) => show.id === +id);
-    if (!result) {
+    try {
+        const showsData = (await axios.get(`http://api.tvmaze.com/shows/${id}`)).data;
+        res.status(200).json(showsData);
+    } catch (error) {
         console.log(`no matching show found for id ${id}`)
         res.status(404).json({
             error: 'no show matching id found in database!',
         });
-    } else {
-        res.status(200).json(result);
     }
 });
 
